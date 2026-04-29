ccde49a2de25dff019917dbdceba54910989df45
Tristan Weech <twminebro@gmail.com>
2026-04-29 09:58:10 -0600

    add(Size) as an Enum

---
+++ src/model/statblock/info/Size.ts
@@ -0,0 +1,8 @@
+export const Size = {
+  Tiny: "Tiny", //2.5 x 2.5 ft
+  Small: "Small", //5 x 5 ft
+  Medium: "Medium", //5 x 5 ft
+  Large: "Large", //10 x 10 ft
+  Huge: "Huge", //15 x 15 ft
+  Gargantuan: "Gargantuan", //20 x 20 ft (or larger)
+} as const;
