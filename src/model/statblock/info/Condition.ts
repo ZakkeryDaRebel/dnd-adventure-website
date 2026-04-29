a6123d2dbe5c71ea7f9a4163edf0880d0f45a1ba
Tristan Weech <twminebro@gmail.com>
2026-04-29 10:47:20 -0600

    add(Condition) as an Enum

---
+++ src/model/statblock/info/Condition.ts
@@ -0,0 +1,17 @@
+export const Condition = {
+  Blinded: "Blinded",
+  Charmed: "Charmed",
+  Deafened: "Deafened",
+  Frightened: "Frightened",
+  Grappled: "Grappled",
+  Incapacitated: "Incapacitated",
+  Invisible: "Invisible",
+  Paralyzed: "Paralyzed",
+  Petrified: "Petrified",
+  Poisoned: "Poisoned",
+  Prone: "Prone",
+  Restrained: "Restrained",
+  Stunned: "Stunned",
+  Unconscious: "Unconscious",
+  Exhaustion: "Exhaustion",
+} as const;
