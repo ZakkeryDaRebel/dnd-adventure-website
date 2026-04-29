081e86a2ef862ac117d07a4a7e43b8bc8dc93c06
Tristan Weech <twminebro@gmail.com>
2026-04-29 09:58:29 -0600

    add(Skills) as an Enum

---
+++ src/model/statblock/info/Skills.ts
@@ -0,0 +1,29 @@
+export const Skills = {
+  // Strength skills
+  Athletics: "Athletics",
+
+  // Dexterity skills
+  Acrobatics: "Acrobatics",
+  SleightOfHand: "Sleight of Hand",
+  Stealth: "Stealth",
+
+  // Intelligence skills
+  Arcana: "Arcana",
+  History: "History",
+  Investigation: "Investigation",
+  Nature: "Nature",
+  Religion: "Religion",
+
+  // Wisdom skills
+  AnimalHandling: "Animal Handling",
+  Insight: "Insight",
+  Medicine: "Medicine",
+  Perception: "Perception",
+  Survival: "Survival",
+
+  // Charisma skills
+  Deception: "Deception",
+  Intimidation: "Intimidation",
+  Performance: "Performance",
+  Persuasion: "Persuasion",
+} as const;
