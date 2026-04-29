c8e30a010d4635f401075d5669960a6bbf3b7146
Tristan Weech <twminebro@gmail.com>
2026-04-29 09:56:31 -0600

    add(Alignment) as an Enum

---
+++ src/model/statblock/info/Alignment.ts
@@ -0,0 +1,11 @@
+export const Alignment = {
+  LG: "Lawful Good",
+  NG: "Neutral Good",
+  CG: "Chaotic Good",
+  LN: "Lawful Neutral",
+  TN: "True Neutral",
+  CN: "Chaotic Neutral",
+  LE: "Lawful Evil",
+  NE: "Neutral Evil",
+  CE: "Chaotic Evil",
+} as const;
