682db3fd2c0dc028b50c70b26df983a49868ab74
Tristan Weech <twminebro@gmail.com>
2026-04-30 10:28:26 -0600

    add(test) for ArmorConstructor

---
+++ tests/model/armor/ArmorConstructor.test.ts
@@ -0,0 +1,79 @@
+import {
+  armorConstructor,
+  plusNumArmorConstructor,
+  ArmorType,
+  Armor,
+} from "../../../src/model/item/armor/Armor";
+import { Cost } from "../../../src/model/item/Cost";
+
+const chainName: string = "Chain Mail";
+const chainPlusName: string = "Chain Mail, +1";
+const chainCost: Cost = { amount: 75, type: "GP" };
+const chainWeight: number = 55;
+const chainAC: number = 16;
+const chainPlusAC: number = 17;
+const chainType: keyof typeof ArmorType = ArmorType.Heavy;
+const chainOther: string =
+  " (Minimum Strength Requirement of 13, Disadvantage on Stealth)";
+
+function compare(
+  armorToCompare: Armor,
+  name: string,
+  cost: Cost,
+  weight: number,
+  ac: number,
+  type: keyof typeof ArmorType,
+  other: string,
+) {
+  expect(armorToCompare.name).toBe(name);
+  expect(armorToCompare.cost).toEqual(cost);
+  expect(armorToCompare.weight).toBe(weight);
+  expect(armorToCompare.baseAC).toBe(ac);
+  expect(armorToCompare.type).toBe(type);
+  expect(armorToCompare.other).toBe(other);
+}
+
+describe("Armor Constructor", () => {
+  test("armorConstructor builds correct armor", () => {
+    const defaultChain = armorConstructor(
+      chainName,
+      chainCost,
+      chainWeight,
+      chainAC,
+      chainType,
+      chainOther,
+    );
+
+    compare(
+      defaultChain,
+      chainName,
+      chainCost,
+      chainWeight,
+      chainAC,
+      chainType,
+      chainOther,
+    );
+  });
+
+  test("plusNumArmorConstructor increases AC and name", () => {
+    const plusChain = plusNumArmorConstructor(
+      1,
+      chainName,
+      chainCost,
+      chainWeight,
+      chainAC,
+      chainType,
+      chainOther,
+    );
+
+    compare(
+      plusChain,
+      chainPlusName,
+      chainCost,
+      chainWeight,
+      chainPlusAC,
+      chainType,
+      chainOther,
+    );
+  });
+});
