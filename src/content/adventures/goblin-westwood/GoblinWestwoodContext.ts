9a9866c7397cc56371743444f8184597ef689aec
Tristan Weech <twminebro@gmail.com>
2026-05-18 11:23:41 -0600

    add(GoblinWestwoodContext.ts) with base information, will need to fill out when I start working on the adventure

---
+++ src/content/adventures/goblin-westwood/GoblinWestwoodContext.ts
@@ -0,0 +1,11 @@
+import type { AdventureContext } from "../../../features/game-state";
+
+export const GOBLIN_WESTWOOD_CONTEXT: AdventureContext = {
+  day: 1,
+  timeOfDay: "Morning",
+  locationId: "start",
+
+  npcs: {},
+
+  shops: {},
+};
