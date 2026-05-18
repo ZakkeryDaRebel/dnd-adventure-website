3c433988e640936c8323c9d582bb4503ddee4602
Tristan Weech <twminebro@gmail.com>
2026-05-18 11:27:11 -0600

    add hooks to update and load the adventure in addition to setting and getting the player's name

--- src/app/providers/GameProvider.tsx
+++ src/app/providers/GameProvider.tsx
@@ -1,11 +1,37 @@
 import { useState } from "react";
 import { GameContext } from "../../features/game-state/";
+import type { AdventureContext } from "../../features/game-state/AdventureContext";
 
 export function GameProvider({ children }: { children: React.ReactNode }) {
   const [playerInitiative, setPlayerInitiative] = useState<number | null>(null);
+  const [adventure, setAdventure] = useState<AdventureContext | null>(null);
+  const [playerName, setPlayerName] = useState<string>("adventurer");
+
+  function loadAdventure(adv: AdventureContext, playerName: string) {
+    setAdventure(adv);
+    setPlayerInitiative(null);
+    setPlayerName(playerName);
+  }
+
+  function updateAdventure(fn: (prev: AdventureContext) => AdventureContext) {
+    setAdventure((prev) => {
+      if (!prev) return prev;
+      return fn(prev);
+    });
+  }
 
   return (
-    <GameContext.Provider value={{ playerInitiative, setPlayerInitiative }}>
+    <GameContext.Provider
+      value={{
+        playerInitiative,
+        setPlayerInitiative,
+        adventure,
+        loadAdventure,
+        updateAdventure,
+        playerName,
+        setPlayerName,
+      }}
+    >
       {children}
     </GameContext.Provider>
   );
