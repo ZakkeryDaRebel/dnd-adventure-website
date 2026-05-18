c2a3a4e4270d1bdfafb70f55d64dd5f02e5239b3
Tristan Weech <twminebro@gmail.com>
2026-05-18 11:24:31 -0600

    update(adventures/index.ts) update barrel file to include the adventure context files

--- src/content/adventures/index.ts
+++ src/content/adventures/index.ts
@@ -3,4 +3,7 @@
 //
 
 export * from "./dark-forest/DarkForestAdventure";
+export * from "./dark-forest/DarkForestContext";
+
 export * from "./goblin-westwood/GoblinWestwoodAdventure";
+export * from "./goblin-westwood/GoblinWestwoodContext";
