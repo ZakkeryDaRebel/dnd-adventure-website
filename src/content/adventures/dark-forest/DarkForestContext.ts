52ccb63044e51fbf644da200f7980f92bcd31f16
Tristan Weech <twminebro@gmail.com>
2026-05-18 11:22:45 -0600

    add(DarkForestContext.ts) base information, not actually filled out yet

---
+++ src/content/adventures/dark-forest/DarkForestContext.ts
@@ -0,0 +1,11 @@
+import type { AdventureContext } from "../../../features/game-state";
+
+export const DARK_FOREST_CONTEXT: AdventureContext = {
+  day: 1,
+  timeOfDay: "Morning",
+  locationId: "start",
+
+  npcs: {},
+
+  shops: {},
+};
