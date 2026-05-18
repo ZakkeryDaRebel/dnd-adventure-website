13616ba563d68e48f4f3dba42aafa6f94a5f84a9
Tristan Weech <twminebro@gmail.com>
2026-05-18 11:26:33 -0600

    update(GameState) to include the player's name and the adventure, in addition to the methods required to update these variables

--- src/features/game-state/GameState.ts
+++ src/features/game-state/GameState.ts
@@ -1,4 +1,12 @@
+import type { AdventureContext } from "./AdventureContext";
+
 export interface GameState {
   playerInitiative: number | null;
   setPlayerInitiative: (n: number | null) => void;
+  playerName: string;
+  setPlayerName: (name: string) => void;
+
+  adventure: AdventureContext | null;
+  loadAdventure: (adv: AdventureContext, playerName: string) => void;
+  updateAdventure: (fn: (prev: AdventureContext) => AdventureContext) => void;
 }
